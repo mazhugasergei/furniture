897c137661431baf857574bda5af65c9eac5ec2d
Markus Wedler <markuswedler8@gmail.com>
2023-06-28 09:49:44 +1000

    Slide 2 paragraph up to min-width: 48.625rem

--- src/components/slides/Slide_02.tsx
+++ src/components/slides/Slide_02.tsx
@@ -18,6 +18,9 @@ export default () => {
         <div className="headline-cont scroll-cont">
           <div className="headline scroll-element" data-delay={.3}>Impeccable<br/>Craftsmanship</div>
         </div>
+        <div className="p-cont scroll-cont">
+          <div className="p scroll-element" data-delay={.2}>Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas facilisis.</div>
+        </div>
       </div>
     </div>
   )
