32ad128e60cba95e0db4342061268936a871a994
Markus Wedler <markuswedler8@gmail.com>
2023-07-13 20:57:09 +1000

    Finish Slide 2 paragraph

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Run ```npm i && npm start```
 
 #### [ CSS ]
 
-* If need certain value to be ```X_px``` when window's width is ```Y_px```, ```X_px + U_px``` when window's width is ```Y_px+V_px``` and so on, use ```calc(X_px + ((100vw - Y_px) / W))``` where ```W = V_px/U_px```
+* If need certain value to be ```X_px``` when window's width is ```Y_px```, ```X_px + U_px``` when window's width is ```Y_px + V_px``` and so on, use ```calc(X_px + ((100vw - Y_px) / W))``` where ```W = V_px/U_px```
--- src/components/slides/Slide_02.tsx
+++ src/components/slides/Slide_02.tsx
@@ -19,7 +19,7 @@ export default () => {
           <div className="headline scroll-element" data-delay={.3}>Impeccable<br/>Craftsmanship</div>
         </div>
         <div className="p-cont scroll-cont">
-          <div className="p scroll-element" data-delay={.2}>Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas facilisis.</div>
+          <div className="p scroll-element" data-delay={.3}>Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas facilisis.</div>
         </div>
       </div>
     </div>
